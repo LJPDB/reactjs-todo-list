4b33555e22b805f7626602e2b0c97c19ef229969
Leonardo Puga De Biase <ing.ljpdb@gmail.com>
2020-05-11 01:16:57 +0200

    adding some styles matching js files

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import "./TodoItem.css"
 
 class TodoItem extends Component {
     render() {
-        return <li><input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)}/> {this.props.todo.title} <button onClick={() => {this.props.handleItemDeletion(this.props.todo.id)}}>Delete</button></li>
+        return (
+            <li>
+                <input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)}/> 
+                    <span className={this.props.todo.completed ? "done":"pending"}>{this.props.todo.title}</span>
+                    <button onClick={() => {this.props.handleItemDeletion(this.props.todo.id)}}>Delete</button>
+                </li>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoComponent from "./components/TodoContainer";
+import TodoContainer from "./components/TodoContainer";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoComponent />
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
